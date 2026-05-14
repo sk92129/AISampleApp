5bbb2f94a87f1032b3285387416ee8bd7e3a9382
SK <1682772+sk92129@users.noreply.github.com>
2026-05-14 16:54:55 -0700

    Initial Commit

---
+++ AISampleApp/AISampleAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  AISampleAppApp.swift
+//  AISampleApp
+//
+//  Created by Sean Kang on 5/14/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AISampleAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
